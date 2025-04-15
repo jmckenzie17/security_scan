19bec02b932075290492d08e622bafc102b00b64
jmckenzie <Justin.McKenzie@vervint.com>
2025-04-15 12:32:21 -0400

    wip

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-print("Hello, world!")
+import libxml2
+import os
+    
+def print_password(password):
+    print(password)
+    
+def print_os_info(os):
+    print(f"[INFO] Environment: {os.environ}")
+
+password = 'password'
 test = "rDshqFRv8/OHDfaBE6/2axCTVaiqj/B2rH8NYSAQb2syuK/ovbrU9SDHDzgjNW1GiIniBgY0THEy+AStDE9zaQ=="
+
+if (True):
+    if (True):
+        print_password(password)
+        print_os_info(os)
