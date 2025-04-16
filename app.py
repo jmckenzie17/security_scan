60012161276af93741ae5f207ca82553ae5100ad
jmckenzie <Justin.McKenzie@vervint.com>
2025-04-16 09:20:52 -0400

    wip

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import libxml2
-import os
+# import libxml2
+# import os
     
-def print_password(password):
-    print(password)
+# def print_password(password):
+#     print(password)
     
-def print_os_info():
-    print(f"[INFO] Environment: {os.environ}")
+# def print_os_info():
+#     print(f"[INFO] Environment: {os.environ}")
 
-password = 'password'
-test = "rDshqFRv8/OHDfaBE6/2axCTVaiqj/B2rH8NYSAQb2syuK/ovbrU9SDHDzgjNW1GiIniBgY0THEy+AStDE9zaQ=="
+# password = 'password'
+# test = "rDshqFRv8/OHDfaBE6/2axCTVaiqj/B2rH8NYSAQb2syuK/ovbrU9SDHDzgjNW1GiIniBgY0THEy+AStDE9zaQ=="
 
-if (True):
-    if (True):
-        print_password(password)
-        print_os_info()
+# if (True):
+#     if (True):
+#         print_password(password)
+#         print_os_info()
